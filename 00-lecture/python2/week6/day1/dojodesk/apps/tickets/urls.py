c2ec9a6f9ee226f9d2c24a2649e1e80c3474c51a
Salvatore Santamaria <sam.santamaria@gmail.com>
2018-12-14 08:34:49 -0800

    Update urls.py

--- 00-lecture/python2/week6/day1/dojodesk/apps/tickets/urls.py
+++ 00-lecture/python2/week6/day1/dojodesk/apps/tickets/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name="index"),
-  url(r'^new/$', views.new, name="new"),
+  url(r'^new/$', views.new, name="new"),#pattern is for tickets/new
   url(r'^create/$', views.create, name="create"),
   url(r'^(?P<ticket_id>\d+)/show/$', views.show, name="show"),
   url(r'^(?P<ticket_id>\d+)/edit/$', views.edit, name="edit"),
