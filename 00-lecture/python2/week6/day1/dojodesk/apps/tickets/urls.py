1c40bbaf94248779cc5a2a4f82c0a3b98641abe2
Salvatore Santamaria <sam.santamaria@gmail.com>
2018-12-14 08:55:04 -0800

    Update urls.py

--- 00-lecture/python2/week6/day1/dojodesk/apps/tickets/urls.py
+++ 00-lecture/python2/week6/day1/dojodesk/apps/tickets/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   url(r'^$', views.index, name="index"),
   url(r'^new/$', views.new, name="new"),#pattern is for /new
-  url(r'^create/$', views.create, name="create"),
+  url(r'^create/$', views.create, name="create"), #name needs to equal the same as namespace in other urls.py file. See s1w7 at 6 minutes.
   url(r'^(?P<ticket_id>\d+)/show/$', views.show, name="show"),
   url(r'^(?P<ticket_id>\d+)/edit/$', views.edit, name="edit"),
   url(r'^(?P<ticket_id>\d+)/update/$', views.update, name="update"),
